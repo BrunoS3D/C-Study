0418f8a48dc5c75a0a03ebbfa3c2a1490aa449ad
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-08 19:41:41 -0300

    hello world script

---
+++ src/helloworld.c
@@ -0,0 +1,10 @@
+// standard input output
+// Algo como "using System;"
+#include <stdio.h>
+
+// função main é a raiz da arvore de chamadas da aplicação
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
