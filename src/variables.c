e1dda18778500901013b232254e89d75c3866db5
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-09 00:46:28 -0300

    Create variables.c

---
+++ src/variables.c
@@ -0,0 +1,12 @@
+// para utilizar o printf
+#include <stdio.h>
+
+int main(void) {
+    // declarei as variaveis
+    // inicializei a variavel "c" realizando uma operação de adição
+    int a = 10, b = 5, c = a + b;
+
+    printf("O resultado de %d mais %d eh igual a %d\n", a, b, c);
+
+    return 0;
+}
